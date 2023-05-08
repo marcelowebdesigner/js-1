6e952946f7261867be841e2a24cb4cd8ee55a06c
marcelowebdesigner <marcelocamusso@hotmail.com>
2023-05-08 13:29:51 -0300

    feature:Ejercicio2.

--- Ejercicio.js
+++ Ejercicio.js
@@ -24,3 +24,26 @@ function piramide(numero) {
     console.log(fila);
   }
 }
+
+/*
+2) Escribir una funcion que reciba 2 array y devuelva un array con todos los elementos que coinciden entre ellos
+
+Ejemplo:
+Array1: ['rojo', 'azul', 'amarillo']
+Array2: ['blanco', 'negro', 'rojo']
+Resultado: ['rojo']
+
+Ejemplo 2:
+Array1: [4, 3, true, 'manzana']
+Array2: ['pera', 3, f alse, true, 3, true]
+Resultado: [3, true]
+*/
+
+//Para cambiar los ejemplos, copie y pegue el contenido de cada array comentado en el ejercicio.
+let Array1 = ['rojo', 'azul', 'amarillo'];
+let Array2 = ['blanco', 'negro', 'rojo'];
+
+//para ejecutar en consola, escriba compararArrays(Array1, Array2)
+function compararArrays(Array1, Array2) {
+  return Array1.filter(element => Array2.includes(element));
+}
